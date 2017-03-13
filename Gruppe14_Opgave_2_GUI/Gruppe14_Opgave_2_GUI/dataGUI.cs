2a26921f66461fd8c94a0efc433d768c9316aa60
2takker <2takker@gmail.com>
2017-03-13 08:40:06 +0100

    Add files via upload

---
+++ Gruppe14_Opgave_2_GUI/Gruppe14_Opgave_2_GUI/dataGUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gruppe14_Opgave_2_GUI
+{
+    public partial class dataGUI : Form
+    {
+        formBS BSform1;
+        formBT BTform1;
+        formBMI BMIform1;
+
+        public dataGUI()
+        {
+            InitializeComponent();
+            
+            BTform1 = new formBT();
+            BMIform1 = new formBMI();
+        }
+
+        private void btnBS_Click(object sender, EventArgs e)
+        {
+            BSform1 = new formBS(dtpStart.Value, dtpSlut.Value);
+            BSform1.ShowDialog();
+        }
+
+        private void btnBT_Click(object sender, EventArgs e)
+        {
+            BTform1.ShowDialog();
+        }
+
+        private void btnBMI_Click(object sender, EventArgs e)
+        {
+            BMIform1.ShowDialog();
+        }
+    }
+}
---
+++ Gruppe14_Opgave_2_GUI/Gruppe14_Opgave_2_GUI/formBS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using logik;
+using DTO;
+
+namespace Gruppe14_Opgave_2_GUI
+{
+    public partial class formBS : Form
+    {
+        LogikStub ls1;
+        List<BS_DTO> BSliste;
+
+
+        public formBS(DateTime startDato, DateTime slutDato)
+        {
+            InitializeComponent();
+            ls1 = new LogikStub();
+            BSliste = ls1.getBS("Ligegyldig string...");
+
+            foreach (var ele in BSliste)
+            {
+                if (ele.getDateTime() >= startDato && ele.getDateTime() <= slutDato)
+                {
+                    chartBS.Series["Blodsukker"].Points.AddXY(ele.getDateTime(), ele.getBloodSugar());
+                }
+            }
+        }
+
+
+    }
+}
