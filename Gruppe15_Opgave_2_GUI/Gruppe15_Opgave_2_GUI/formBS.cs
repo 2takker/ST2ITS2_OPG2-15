a53f71cbe657e736fc9c6b075857a49ede8bb39a
2takker <2takker@gmail.com>
2017-04-03 09:06:17 +0200

    Fjernet resten af gruppe14

---
+++ Gruppe15_Opgave_2_GUI/Gruppe15_Opgave_2_GUI/formBS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DTO;
+using Logik;
+
+namespace Gruppe15_Opgave_2_GUI
+{
+    public partial class formBS : Form
+    {
+        logikLag lk;
+        List<BS_DTO> BSliste = new List<BS_DTO>();
+
+
+
+        public formBS(DateTime startDato, DateTime slutDato, string cpr)
+        {
+            InitializeComponent();
+            lk = new logikLag();
+            BSliste = lk.getPatientBS(cpr);
+
+            foreach (var ele in BSliste)
+            {
+                if (ele.getDateTime() >= startDato && ele.getDateTime() <= slutDato)
+                {
+                    chartBS.Series["Blodsukker"].Points.AddXY(ele.getDateTime(), ele.getBloodSugar());
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Gruppe15_Opgave_2_GUI/Gruppe15_Opgave_2_GUI/formBT.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Gruppe15_Opgave_2_GUI
+{
+    partial class formBT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartBT = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartBT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartBT
+            // 
+            chartArea1.AxisX.Title = "Dato";
+            chartArea1.AxisY.Title = "mmHg";
+            chartArea1.Name = "ChartArea1";
+            this.chartBT.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.chartBT.Legends.Add(legend1);
+            this.chartBT.Location = new System.Drawing.Point(59, 88);
+            this.chartBT.Name = "chartBT";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Systole";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Diastole";
+            this.chartBT.Series.Add(series1);
+            this.chartBT.Series.Add(series2);
+            this.chartBT.Size = new System.Drawing.Size(1118, 815);
+            this.chartBT.TabIndex = 0;
+            this.chartBT.Text = "chart1";
+            // 
+            // formBT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1289, 963);
+            this.Controls.Add(this.chartBT);
+            this.Name = "formBT";
+            this.Text = "Blodtryk";
+            ((System.ComponentModel.ISupportInitialize)(this.chartBT)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartBT;
+    }
+}
